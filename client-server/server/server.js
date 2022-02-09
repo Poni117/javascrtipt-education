bff8f68273f1d355ac6d96ad622358afcff005bf
MestraSoft <mestrasoft@gmail.com>
2022-02-09 13:42:37 +0300

    refactoring

--- client-server/server/server.js
+++ client-server/server/server.js
@@ -5,6 +5,7 @@ const server = new WebSocketServer({port: 8080});
 server.on('connection', (clientSocket) => {
 
     clientSocket.on('message', (data) => {
+        
         console.log('Client: %s', data);
 
         clientSocket.send('Hi Client');
