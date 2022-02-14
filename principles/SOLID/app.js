6342b69a3acb66c9297a982bed6a48750d113130
MestraSoft <mestrasoft@gmail.com>
2022-02-14 15:27:27 +0300

    add Redux

--- principles/SOLID/app.js
+++ principles/SOLID/app.js
@@ -1,8 +1,19 @@
 "use strict";
 exports.__esModule = true;
 var enveronment_1 = require("./enveronment");
-function Start() {
-    var enveronment = new enveronment_1["default"]();
-    enveronment.Start();
-}
-Start();
+var THREE = require("three");
+var Game = /** @class */ (function () {
+    function Game() {
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1000);
+        this.renderer = new THREE.WebGLRenderer({ antalias: true });
+        this.Start();
+    }
+    Game.prototype.Start = function () {
+        document.body.appendChild(this.renderer.domElement);
+        var environment = new enveronment_1["default"]();
+        environment.Start();
+    };
+    return Game;
+}());
+new Game();
--- principles/SOLID/app.ts
+++ principles/SOLID/app.ts
@@ -1,10 +1,33 @@
 import Environment from "./enveronment";
+import * as THREE from 'three';
 
-function Start(){
+class Game {
+
+    scene: THREE.Scene;
+
+    camera: THREE.PerspectiveCamera;
+
+    renderer: THREE.WebGLRenderer;
+
+    constructor(){
+
+      this.scene = new THREE.Scene();
+
+      this.camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1000);
+
+      this.renderer = new THREE.WebGLRenderer({antalias: true});
+
+      this.Start();
+    }
     
-    const enveronment: Environment = new Environment();
+    Start(): void{
+        
+        document.body.appendChild(this.renderer.domElement);
 
-    enveronment.Start();
+        const environment: Environment = new Environment();
+    
+        environment.Start();
+    }
 }
 
-Start();
+new Game()
